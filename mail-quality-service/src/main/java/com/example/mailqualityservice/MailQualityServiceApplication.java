1fc7510cb288b8e251e7bf24428e8c9b7921ddfc
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-10-05 01:05:06 +0200

    Client two quality mail

---
+++ mail-quality-service/src/main/java/com/example/mailqualityservice/MailQualityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailqualityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailQualityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailQualityServiceApplication.class, args);
+    }
+
+}
---
+++ mail-quality-service/src/test/java/com/example/mailqualityservice/MailQualityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailqualityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailQualityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
