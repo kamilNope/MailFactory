1284d9db8fca4d6b9c123136d3e3da14537cc2d5
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-10-04 23:54:56 +0200

    Initial commit service one

---
+++ mail-fast-service/src/main/java/com/example/mailfastservice/MailFastServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailfastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailFastServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailFastServiceApplication.class, args);
+    }
+
+}
---
+++ mail-fast-service/src/test/java/com/example/mailfastservice/MailFastServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailfastservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailFastServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
