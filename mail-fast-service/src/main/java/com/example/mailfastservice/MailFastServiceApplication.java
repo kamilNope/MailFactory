19af723fe91a1f24ef62e65dd604b66242848b85
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-10-05 14:03:15 +0200

    Client fast mail setup

--- mail-fast-service/src/main/java/com/example/mailfastservice/MailFastServiceApplication.java
+++ mail-fast-service/src/main/java/com/example/mailfastservice/MailFastServiceApplication.java
@@ -1,13 +1,29 @@
 package com.example.mailfastservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class MailFastServiceApplication {
 
+    @Autowired
+    EurekaClient eurekaClient;
+
     public static void main(String[] args) {
         SpringApplication.run(MailFastServiceApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String serviceInstanceName() {
+        return "Hello world!\n\n" + eurekaClient.getInstanceRemoteStatus() + "\n" + eurekaClient.getEurekaClientConfig();
     }
 
 }
